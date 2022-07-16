89689c4df0d7b73aad5831220144d72c881a2420
Patrycja Lamik <patrycjalamik94@gmail.com>
2022-07-17 00:42:44 +0200

    zadanie 8

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,28 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[mobile-menu]"),
-      closeMenuBtn: document.querySelector("[mobile-menu__btn--open]"),
-      modal: document.querySelector("[mobile-menu__btn--close]"),
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+  
+    const toggleMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
+  
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
     };
   
-    refs.openMenuBtn.addEventListener("click", toggleModal);
-    refs.closeMenuBtn.addEventListener("click", toggleModal);
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
   
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
   })();
